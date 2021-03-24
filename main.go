196c1edecee4ffc2b8ff50db2ec362a8f7398a9f
Reno D Hermawan <reno.d.hermawan@gmail.com>
2021-03-24 10:04:34 +0700

    adding odd and even number function

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	var input string
+	fmt.Print("Input number: ")
+	fmt.Scan(&input)
+
+	number, err := oddAndEvenNumbers(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(number)
 }
 
-func oddAndEvenNumbers() {
+func oddAndEvenNumbers(input string) (string, error) {
+	i, err := strconv.Atoi(input)
+	if err != nil {
+		return "", fmt.Errorf("Input isn't number")
+	}
 
+	if i%2 == 0 {
+		return "even number", nil
+	} else {
+		return "odd number", nil
+	}
 }
